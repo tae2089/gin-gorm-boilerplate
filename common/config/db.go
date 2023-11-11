42a904469db087d2ee303b3eb0c084803f27cea0
tae2089 <tae2089002@gmail.com>
2023-11-11 20:16:43 +0900

    :safety_vest: Add conditioanl that checking db is nil

--- common/config/db.go
+++ common/config/db.go
@@ -13,21 +13,23 @@ var client *gorm.DB
 
 func LoadDBConfig() ConfigOption {
 	return func() {
-		logger.Info("initializing database")
-		dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Asia/Seoul",
-			os.Getenv("DB_HOST"),
-			os.Getenv("DB_USER"),
-			os.Getenv("DB_PASSWORD"),
-			os.Getenv("DB_NAME"),
-			os.Getenv("DB_PORT"),
-		)
+		if client == nil {
+			logger.Info("initializing database")
+			dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Asia/Seoul",
+				os.Getenv("DB_HOST"),
+				os.Getenv("DB_USER"),
+				os.Getenv("DB_PASSWORD"),
+				os.Getenv("DB_NAME"),
+				os.Getenv("DB_PORT"),
+			)
 
-		db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
-		client = db
-		if err != nil {
-			panic(err)
+			db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+			client = db
+			if err != nil {
+				panic(err)
+			}
+			logger.Info("initialized database")
 		}
-		logger.Info("initialized database")
 	}
 }
 
