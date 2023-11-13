392e23bf1a2403d7c9f7278be58cadc492e0bffd
tae2089 <tae2089002@gmail.com>
2023-11-13 20:13:04 +0900

    create func about uploding file to cloud

---
+++ common/util/s3.go
@@ -0,0 +1,70 @@
+package util
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"path/filepath"
+	"time"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
+	uuid "github.com/satori/go.uuid"
+	"github.com/tae2089/gin-boilerplate/common/config"
+)
+
+type CloudStorageUtil interface {
+	UploadFileToCloud(file io.Reader, fileName string) (string, error)
+}
+
+type S3StorageUtil struct {
+	s3Client *s3.Client
+	bucket   string
+}
+
+func NewS3StorageUtil() CloudStorageUtil {
+	cfg := config.NewAwsConfig()
+	client := s3.NewFromConfig(cfg)
+	return &S3StorageUtil{
+		s3Client: client,
+	}
+}
+
+func (s *S3StorageUtil) UploadFileToCloud(file io.Reader, fileName string) (string, error) {
+	date, err := formatDate()
+	if err != nil {
+		panic(err)
+	}
+	uuid := uuid.NewV4()
+	ext := filepath.Ext(fileName)
+	_, err = s.s3Client.PutObject(context.Background(), &s3.PutObjectInput{
+		Key:    aws.String(fmt.Sprintf("%s/%s%s", date, uuid.String(), ext)),
+		Bucket: &s.bucket,
+		Body:   file,
+	})
+	if err != nil {
+		return "", err
+	}
+
+	objectURL := fmt.Sprintf("https://%s.s3.ap-northeast-2.amazonaws.com/%s/%s%s", s.bucket, date, uuid.String(), ext)
+	return objectURL, nil
+}
+
+func formatDate() (string, error) {
+	location, err := time.LoadLocation("Asia/Seoul")
+	if err != nil {
+		return "", err
+	}
+
+	currentTime := time.Now().In(location)
+	year := currentTime.Year()
+	month := int(currentTime.Month())
+	day := currentTime.Day()
+
+	// 월과 일이 한 자리 수인 경우 앞에 0을 추가하여 두 자리로 만듦
+	monthStr := fmt.Sprintf("%02d", month)
+	dayStr := fmt.Sprintf("%02d", day)
+
+	dateStr := fmt.Sprintf("%d%s%s", year, monthStr, dayStr)
+	return dateStr, nil
+}
