ff73a29584228458730b58a3a7288321b5bec416
tae2089 <tae2089002@gmail.com>
2023-11-15 20:36:24 +0900

    update github config type custom config to oauth2 config

--- oauth/service/github.go
+++ oauth/service/github.go
@@ -1,24 +1,15 @@
 package service
 
 import (
-	"bytes"
-	"encoding/json"
-	"fmt"
+	"context"
 	"io"
-	"net/http"
 
 	"github.com/tae2089/gin-boilerplate/common/config"
-	"github.com/tae2089/gin-boilerplate/oauth/domain"
+	"golang.org/x/oauth2"
 )
 
-type OauthService interface {
-	GetAccessToken(code string) (string, error)
-	GetUserInfo(accessToken string) (string, error)
-}
-
 type GithubOauth struct {
-	ClientID     string `json:"client_id"`
-	ClientSecret string `json:"client_secret"`
+	oauth2.Config
 }
 
 func NewGithubOauthService() OauthService {
@@ -27,74 +18,25 @@ func NewGithubOauthService() OauthService {
 		return nil
 	}
 	return &GithubOauth{
-		ClientID:     githubOauthConfig.ClientID,
-		ClientSecret: githubOauthConfig.ClientSecret,
+		githubOauthConfig,
 	}
 }
 
-func (g *GithubOauth) GetAccessToken(code string) (string, error) {
-
-	// Set us the request body as JSON
-	requestBodyMap := map[string]string{
-		"client_id":     g.ClientID,
-		"client_secret": g.ClientSecret,
-		"code":          code,
+func (g *GithubOauth) GetAccessToken(code string) (*oauth2.Token, error) {
+	token, err := g.Exchange(context.Background(), code)
+	if err != nil {
+		return nil, err
 	}
-	requestJSON, _ := json.Marshal(requestBodyMap)
-
-	// POST request to set URL
-	req, reqerr := http.NewRequest(
-		"POST",
-		"https://github.com/login/oauth/access_token",
-		bytes.NewBuffer(requestJSON),
-	)
-	if reqerr != nil {
-		return "", reqerr
-	}
-	req.Header.Set("Content-Type", "application/json")
-	req.Header.Set("Accept", "application/json")
-
-	// Get the response
-	resp, resperr := http.DefaultClient.Do(req)
-	if resperr != nil {
-		return "", reqerr
-	}
-
-	// Response body converted to stringified JSON
-	respbody, _ := io.ReadAll(resp.Body)
-
-	// Convert stringified JSON to a struct object of type githubAccessTokenResponse
-	var ghresp domain.GithubAccessTokenResponse
-	json.Unmarshal(respbody, &ghresp)
-
-	// Return the access token (as the rest of the
-	// details are relatively unnecessary for us)
-	return ghresp.AccessToken, nil
+	return token, nil
 }
 
-func (g *GithubOauth) GetUserInfo(accessToken string) (string, error) {
+func (g *GithubOauth) GetUserInfo(token *oauth2.Token) (string, error) {
 
-	// Get request to a set URL
-	req, reqerr := http.NewRequest(
-		"GET",
-		"https://api.github.com/user",
-		nil,
-	)
-	if reqerr != nil {
-		return "", reqerr
+	client := g.Client(context.Background(), token)
+	resp, err := client.Get(OAUTH_GITHUB_URL)
+	if err != nil {
+		return "", err
 	}
-
-	// Set the Authorization header before sending the request
-	// Authorization: token XXXXXXXXXXXXXXXXXXXXXXXXXXX
-	authorizationHeaderValue := fmt.Sprintf("token %s", accessToken)
-	req.Header.Set("Authorization", authorizationHeaderValue)
-
-	// Make the request
-	resp, resperr := http.DefaultClient.Do(req)
-	if resperr != nil {
-		return "", reqerr
-	}
-
 	// Read the response as a byte slice
 	respbody, _ := io.ReadAll(resp.Body)
 
