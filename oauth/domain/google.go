91afa04d79dac7532c15d63f276789c09cfae3ae
tae2089 <tae2089002@gmail.com>
2023-11-16 00:01:04 +0900

    define google user info

--- oauth/domain/google.go
+++ oauth/domain/google.go
@@ -2,4 +2,13 @@ package domain
 
 type GoogleAccessTokenResponse struct{}
 
-type GoogleUserInfo struct{}
+type GoogleUserInfo struct {
+	ID            string `json:"id"`
+	Email         string `json:"email"`
+	Name          string `json:"name"`
+	GivenName     string `json:"given_name"`
+	FamilyName    string `json:"family_name"`
+	Picture       string `json:"picture"`
+	Locale        string `json:"locale"`
+	VerifiedEmail bool   `json:"verified_email"`
+}
