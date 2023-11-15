887977fd7c670958ac6d9bf90c89a115977ea5eb
tae2089 <tae2089002@gmail.com>
2023-11-15 21:09:03 +0900

    add google login callback

--- oauth/handler/oauth_handler.go
+++ oauth/handler/oauth_handler.go
@@ -10,11 +10,13 @@ import (
 
 type OauthHandler struct {
 	githubService oauth.OauthService
+	googleService oauth.OauthService
 }
 
-func NewOauthHandler(githubService oauth.OauthService) *OauthHandler {
+func NewOauthHandler(githubService, googleService oauth.OauthService) *OauthHandler {
 	return &OauthHandler{
 		githubService: githubService,
+		googleService: googleService,
 	}
 }
 
@@ -34,4 +36,22 @@ func (o *OauthHandler) GithubLoginCallback(c *gin.Context) {
 	json.Unmarshal([]byte(userInfo), &githubUserInfo)
 	// c.SetCookie("access_token", accessToken, 3600, "/", "localhost", false, true)
 	c.JSON(201, gin.H{"isSuccess": true, "userInfo": githubUserInfo})
+}
+
+func (o *OauthHandler) GoogleLoginCallback(c *gin.Context) {
+	code := c.Query("code")
+	oauthToken, err := o.googleService.GetAccessToken(code)
+	if err != nil {
+		c.JSON(400, gin.H{"error": err.Error()})
+		return
+	}
+	userInfo, err := o.googleService.GetUserInfo(oauthToken)
+	if err != nil {
+		c.JSON(400, gin.H{"error": err.Error()})
+		return
+	}
+	var googleUserInfo domain.GoogleUserInfo
+	json.Unmarshal([]byte(userInfo), &googleUserInfo)
+	// c.SetCookie("access_token", accessToken, 3600, "/", "localhost", false, true)
+	c.JSON(201, gin.H{"isSuccess": true, "userInfo": googleUserInfo})
 }
