4419e70490df8806d4d5e6f4e9f35d9482aeb6d1
tae2089 <tae2089002@gmail.com>
2023-11-19 10:34:49 +0900

    run notification channel

--- run/main.go
+++ run/main.go
@@ -13,6 +13,7 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/tae2089/gin-boilerplate/common/config"
 	"github.com/tae2089/gin-boilerplate/common/router"
+	"github.com/tae2089/gin-boilerplate/notification"
 	"github.com/tae2089/gin-boilerplate/user/model"
 )
 
@@ -24,6 +25,7 @@ func main() {
 
 	db.AutoMigrate(&model.User{})
 	router.SetupRouter(e, db, jwtKey)
+	notification.Listen(notification.UseLoggerProvider)
 	// e.Run()
 	e.Use(cors.New(cors.Config{
 		AllowOrigins:     []string{"*"},
