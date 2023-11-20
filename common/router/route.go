6f3c4e570940b0280cdb264decd402c8cc8d953e
tae2089 <tae2089002@gmail.com>
2023-11-20 18:59:20 +0900

    create middleware functions and apply middlewares

--- common/router/route.go
+++ common/router/route.go
@@ -1,6 +1,9 @@
 package router
 
 import (
+	"time"
+
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/tae2089/gin-boilerplate/common/domain"
 	"github.com/tae2089/gin-boilerplate/common/middleware"
@@ -13,11 +16,11 @@ import (
 func SetupRouter(e *gin.Engine, db *gorm.DB, jwtKey domain.JwtKey) {
 
 	e.LoadHTMLGlob("templates/*")
-	e.Use(middleware.ErrorHandler(notification.GetErrorChan()))
+	useCorsMiddleware(e)
+	useErrorSenderMiddleware(e)
 	jwtUtil := util.NewJwtUtil(jwtKey)
 
 	healthRouter := e.Group("/")
-	healthRouter.Use(middleware.CheckAccessToken(jwtUtil))
 	newHealthRouter(healthRouter)
 
 	userRouter := e.Group("/user")
@@ -27,4 +30,19 @@ func SetupRouter(e *gin.Engine, db *gorm.DB, jwtKey domain.JwtKey) {
 	oauthRouter := e.Group("/oauth")
 	newOauthRouter(oauthRouter, jwtUtil, userRepository)
 
+}
+
+func useCorsMiddleware(e *gin.Engine) {
+	e.Use(cors.New(
+		cors.Config{
+			AllowOrigins:     []string{"*"},
+			AllowMethods:     []string{"*"},
+			AllowHeaders:     []string{"*"},
+			AllowCredentials: true,
+			MaxAge:           12 * time.Hour,
+		}))
+}
+
+func useErrorSenderMiddleware(e *gin.Engine) {
+	e.Use(middleware.ErrorHandler(notification.GetErrorChan()))
 }
